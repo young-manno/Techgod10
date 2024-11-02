2d3a980602335015d13a049bfbd24e3fb8a63c71
SHADOWHACKERS-lab <okwolisolomon92@gmail.com>
2024-11-02 14:33:29 +0100

    Create config.js

---
+++ media/config.js
@@ -0,0 +1,58 @@
+/*
+
+# Base By 饾暥饾枎饾枔饾枌 饾暰饾枂饾枓
+# Owner ? : 饾暥饾枎饾枔饾枌 饾暰饾枂饾枓
+!- do not delete this credit
+# decode by Tech God
+
+*/
+
+global.prefa = ['','!','.',',','馃悿','馃椏']
+global.owner = ['917466008456']
+global.ownMain = '917466008456'
+global.NamaOwner = 'Tevh God' //
+global.sessionName = 'session'
+global.connect = true // 
+global.namabot = 'Tech God- 10' //
+global.author = 'Tech God' //
+global.packname = 'ByTechGod10' //
+global.url1 = 'https://whatsapp.com/channel/0029Va9Ufzi8kyyEnEHvOm1h' //
+global.url2 = 'https://whatsapp.com/channel/0029Va9Ufzi8kyyEnEHvOm1h' //
+global.linkgc = 'https://whatsapp.com/channel/0029Va9Ufzi8kyyEnEHvOm1h'
+global.delayjpm = 3500
+//Panel
+global.domain = 'https://guru.sellerpanell.me' // your domian
+global.apikey = 'ptla_aRq7aFBbQowvPwLChvjNLX5uE0rYZ6dUdGbFIhqUwUg' // 
+global.capikey = 'ptlc_mI4q2CFLyxYSG9lqtpWVJSkqtxbQvZyheemGqW56VUH' // 
+global.eggsnya = '15' // 
+global.location = '1' // 
+
+global.mess = { // 
+ingroup: 'This feature can only be used in groups.',
+admin: 'This feature is specifically for group admins.',
+notadmin: "The bot must be an admin first",
+owner: 'You are not owner.',
+premium: 'You are not a premium user.',
+seller: 'This feature can only be used by resellers and owners.',
+usingsetpp: `Setpp can only be used by the owner, do you think Im stupid?`,
+wait: '*Waiting for processing*',
+success: 'Success sent by Tech God 10',
+bugrespon: `Processs.....`
+}
+
+global.nick = { // Custom Sesuka Lu
+aaa: "鈥忊€嶵覊聽E覊聽C覊聽H覊聽G覊聽O覊D覊聽 1覊聽0",
+bbb: "鈨癈覊聽R覊聽A覊聽S覊聽H覊聽毯",
+ccc: "M覊聽A覊聽S覊聽T覊聽E覊聽R覊聽 M覊聽I覊聽N覊聽D覊聽馃敟 锟�? 鈥屸€屸€屸€屸€屸€屸€屸€屸€屸€岋拷?",
+ddd: "K覊聽I覊聽N覊聽G覊聽 T覊聽E覊聽C覊聽H覊聽G覊聽O覊D聽馃寧",
+eee: "A覊聽L覊聽L覊聽 H覊聽A覊聽I覊聽L覊聽 T覊聽E覊聽C覊聽H覊聽G覊聽O覊 D聽覊聽馃敟馃敟"
+}
+
+global.autOwn = 'req(62-8S57547ms11).287p'
+let file = require.resolve(__filename)
+require('fs').watchFile(file, () => {
+	require('fs').unwatchFile(file)
+	console.log('\x1b[0;32m'+__filename+' \x1b[1;32mupdated!\x1b[0m')
+	delete require.cache[file]
+	require(file)
+})
